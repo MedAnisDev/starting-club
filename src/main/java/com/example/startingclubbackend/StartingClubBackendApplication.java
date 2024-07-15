542902e370ffb7c28a5feb20964f1d95baa8014b
MedAnisDev <mohamedanisdev@gmail.com>
2024-07-15 11:12:26 +0100

    Initial commit

---
+++ src/main/java/com/example/startingclubbackend/StartingClubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.startingclubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingClubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartingClubBackendApplication.class, args);
+	}
+
+}
